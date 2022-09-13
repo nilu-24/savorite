80139f4c251afc45898cd9ea31b62198ae0aabdf
Fardin Abdullah <fardindino@gmail.com>
2022-09-13 13:07:52 -0400

    set up apollo client

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ApolloProvider from './ApolloProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ApolloProvider
 );
 
 // If you want to start measuring performance in your app, pass a function
